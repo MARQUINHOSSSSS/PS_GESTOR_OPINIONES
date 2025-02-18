ace157e9c25355178fafa545cbb1cc69250581e2
PXXxBADBOYxXXP <marcojosepro2007@gmail.com>
2025-02-18 15:54:33 -0600

    conexion del servidor y base de datos

---
+++ configs/server.js
@@ -0,0 +1,48 @@
+'use strict'
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { dbConexion } from "./mongo.js"
+//import userRoutes from '../src/users/user.routes.js';
+//import authRoutes from '../src/auth/auth.routes.js';
+//import publicationRoutes from '../src/posts/posts.routes.js';
+//import commentRoutes from '../src/comments/comments.routes.js'
+//import apiLimiter from "../src/middlewares/rate-limit-validator.js"
+
+const middlewares = (app) =>{
+    app.use(express.json())
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan("dev"))
+    //app.use(apiLimiter)
+}
+
+const routes = (app) =>{
+    //app.use("/opinionmanager/v1/auth", authRoutes)
+    //app.use("/opinionmanager/v1/user", userRoutes)
+    // app.use("/opinionmanager/v1/products" , publicationRoutesRoutes)
+    //app.use("/opinionmanager/v1/category", commentRoutesRoutes)
+}
+
+const conectarDB = async () =>{
+    try{
+        await dbConexion()
+    }catch(e){
+        console.log(`DATABASE CONNECTION FAILED: ${e}`)
+        process.exit(1)    
+    }
+}
+
+export const inicioServidor = () =>{
+    const app = express()
+    try{
+        middlewares(app)
+        conectarDB()
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`SERVER CORRIENDO EN EL PUERTO ${process.env.PORT}`)
+    }catch(err){
+        console.log(`FALLO INICIO DEL SERVIDOR: ${err}`) 
+    }
+}
